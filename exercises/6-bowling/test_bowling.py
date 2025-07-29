743d349119cc2f88b7d35002c70a67b8707e7236
Aliou <aliou.diabytonygarnier@gmail.com>
2025-07-29 16:55:43 +0200

    test_all_ones

--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -5,3 +5,8 @@ def test_gutter_game():
     for _ in range(20):
         g.roll(0)
     assert g.score() == 0
+def test_all_ones():
+    g = Game()
+    for _ in range(20):
+        g.roll(1)
+    assert g.score() == 20
