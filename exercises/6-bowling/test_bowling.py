8f52b28aff84eb62870097bc5e7c3b9cd9d12041
Aliou <aliou.diabytonygarnier@gmail.com>
2025-07-30 10:14:10 +0200

    implement an empty party test_mixed_game

--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -29,31 +29,31 @@ def test_spare_in_last_frame():
     g = Game()
     for _ in range(9):
         g.roll("9-")  # 9 + 0 = 9
-    g.roll("5/")  # spare au dernier frame
-    g.roll("5")   # bonus
+    g.roll("5/")
+    g.roll("5")
     assert g.score() == 9 * 9 + 10 + 5  # 90 + 15 = 105
 
 def test_strike_in_last_frame():
     g = Game()
     for _ in range(9):
         g.roll("9-")
-    g.roll("X")   # strike au dernier frame
-    g.roll("5")   # premier bonus
-    g.roll("3")   # deuxième bonus
+    g.roll("X")
+    g.roll("5")
+    g.roll("3")
     assert g.score() == 9 * 9 + 10 + 5 + 3  # 90 + 18 = 108
 
 def test_perfect_game():
     g = Game()
-    g.roll("XXXXXXXXXXXX")  # 12 strikes
+    g.roll("XXXXXXXXXXXX")
     assert g.score() == 300
 
 def test_all_spares_with_5_bonus():
     g = Game()
-    g.roll("5/5/5/5/5/5/5/5/5/5/5")  # 10 spares + 5 bonus
+    g.roll("5/5/5/5/5/5/5/5/5/5/5")
     assert g.score() == 150
 
 def test_mixed_game():
     g = Game()
     g.roll("X7/9-X-88/-6XXX81")
-    assert g.score() == 167  # valeur calculée selon les règles officielles
+    assert g.score() == 167  #
 
