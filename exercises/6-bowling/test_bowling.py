0b49d5b20ede379e083ee2410890bcb70120be2c
Aliou <aliou.diabytonygarnier@gmail.com>
2025-07-30 10:11:05 +0200

    implement an empty party perfect_game

--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -25,4 +25,27 @@ def test_all_strikes():
         g.roll("X")
     assert g.score() == 300
 
+def test_spare_in_last_frame():
+    g = Game()
+    for _ in range(9):
+        g.roll("9-")  # 9 + 0 = 9
+    g.roll("5/")  # spare au dernier frame
+    g.roll("5")   # bonus
+    assert g.score() == 9 * 9 + 10 + 5  # 90 + 15 = 105
+
+def test_strike_in_last_frame():
+    g = Game()
+    for _ in range(9):
+        g.roll("9-")
+    g.roll("X")   # strike au dernier frame
+    g.roll("5")   # premier bonus
+    g.roll("3")   # deuxième bonus
+    assert g.score() == 9 * 9 + 10 + 5 + 3  # 90 + 18 = 108
+
+def test_perfect_game():
+    g = Game()
+    g.roll("XXXXXXXXXXXX")  # 12 strikes
+    assert g.score() == 300
+
+
 
