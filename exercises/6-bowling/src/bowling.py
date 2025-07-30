6543f089ff86b28d79241d07d1cc2f7064e1b8ab
Aliou <aliou.diabytonygarnier@gmail.com>
2025-07-30 10:10:03 +0200

    implement an empty party all strikes

--- exercises/6-bowling/src/bowling.py
+++ exercises/6-bowling/src/bowling.py
@@ -2,13 +2,29 @@ class Game:
     def __init__(self):
         self.rolls = []
 
-    def roll(self, frame: str):
-        for char in frame:
-            if char == '-':
+    def roll(self, frame_result: str):
+        for i, char in enumerate(frame_result):
+            if char == 'X':
+                self.rolls.append(10)
+            elif char == '-':
                 self.rolls.append(0)
-            else:
-                raise ValueError(f"Caractère non supporté : {char}")
+            elif char.isdigit():
+                self.rolls.append(int(char))
+            elif char == '/':
+                prev = self.rolls[-1]
+                self.rolls.append(10 - prev)
 
     def score(self):
-        return sum(self.rolls)
-
+        score = 0
+        i = 0
+        for frame in range(10):
+            if self.rolls[i] == 10:  # strike
+                score += 10 + self.rolls[i + 1] + self.rolls[i + 2]
+                i += 1
+            elif self.rolls[i] + self.rolls[i + 1] == 10:  # spare
+                score += 10 + self.rolls[i + 2]
+                i += 2
+            else:
+                score += self.rolls[i] + self.rolls[i + 1]
+                i += 2
+        return score
--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -5,3 +5,24 @@ def test_gutter_game():
     for _ in range(10):
         g.roll("--")
     assert g.score() == 0
+
+def test_all_nines_and_misses():
+    g = Game()
+    for _ in range(10):
+        g.roll("9-")
+    assert g.score() == 90
+
+def test_all_spares():
+    g = Game()
+    for _ in range(10):
+        g.roll("5/")
+    g.roll("5")
+    assert g.score() == 150
+
+def test_all_strikes():
+    g = Game()
+    for _ in range(12):
+        g.roll("X")
+    assert g.score() == 300
+
+
