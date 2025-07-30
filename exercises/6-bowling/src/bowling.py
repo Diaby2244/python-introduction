dd4a46e4783ff7f576c9d618f128a33917bde62b
Aliou <aliou.diabytonygarnier@gmail.com>
2025-07-30 14:24:08 +0200

    strike_in_last_frame

--- exercises/6-bowling/src/bowling.py
+++ exercises/6-bowling/src/bowling.py
@@ -5,18 +5,18 @@ class Game:
             self.roll(rolls)
 
     def roll(self, rolls: str):
-        # Supprimer les espaces et stocker les caractères individuellement
+        # Supprimer les espaces et stocker chaque caractère
         self.rolls = [char for char in rolls.replace(" ", "")]
 
     def score(self) -> int:
         total = 0
         i = 0
 
-        def char_to_value(c):
+        def char_to_value(c, prev=None):
             if c == 'X':
                 return 10
             elif c == '/':
-                return 10 - char_to_value(self.rolls[i - 1])
+                return 10 - char_to_value(prev or self.rolls[i - 1])
             elif c == '-':
                 return 0
             else:
@@ -24,7 +24,7 @@ class Game:
 
         for frame in range(10):
             if self.rolls[i] == 'X':
-                total += 10 + char_to_value(self.rolls[i + 1]) + char_to_value(self.rolls[i + 2])
+                total += 10 + char_to_value(self.rolls[i + 1]) + char_to_value(self.rolls[i + 2], self.rolls[i + 1])
                 i += 1
             elif self.rolls[i + 1] == '/':
                 total += 10 + char_to_value(self.rolls[i + 2])
--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -20,3 +20,12 @@ def test_perfect_game():
 def test_all_nine_and_miss():
     g = Game("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-")
     assert g.score() == 90
+
+def test_spare_in_last_frame():
+    g = Game("-- -- -- -- -- -- -- -- -- 5/5")
+    assert g.score() == 15  # 10e frame = 10 + 5
+
+
+def test_strike_in_last_frame():
+    g = Game("-- -- -- -- -- -- -- -- -- X53")
+    assert g.score() == 18  # 10e frame = 10 + 5 + 3
