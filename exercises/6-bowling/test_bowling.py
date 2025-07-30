99fcab91582616347501d10288dee8080fb9fde3
Aliou <aliou.diabytonygarnier@gmail.com>
2025-07-30 09:27:23 +0200

    implement empty party

--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -5,24 +5,3 @@ def test_gutter_game():
     for _ in range(20):
         g.roll(0)
     assert g.score() == 0
-def test_all_ones():
-    g = Game()
-    for _ in range(20):
-        g.roll(1)
-    assert g.score() == 20
-def test_one_spare():
-    g = Game()
-    g.roll(5)
-    g.roll(5)  # spare
-    g.roll(3)  # bonus pour le spare
-    for _ in range(17):
-        g.roll(0)
-    assert g.score() == 13  # 10 + 3
-def test_one_strike():
-    g = Game()
-    g.roll(10)  # strike
-    g.roll(3)
-    g.roll(4)
-    for _ in range(16):
-        g.roll(0)
-    assert g.score() == 17  # 10 + 3 + 4
