a54cde626c47c89110645a01b23c2331f24bd2d7
Aliou <aliou.diabytonygarnier@gmail.com>
2025-07-30 14:12:14 +0200

    all_nine_and_miss

--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -2,10 +2,21 @@ from src.bowling import Game
 
 def test_gutter_game():
     g = Game("-- -- -- -- -- -- -- -- -- -- ")
-
     assert g.score() == 0
-
 
 def test_all_ones():
     g = Game("11 11 11 11 11 11 11 11 11 11")
     assert g.score() == 20
+
+def test_all_spares_with_5():
+    g = Game("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5")
+    assert g.score() == 150
+
+
+def test_perfect_game():
+    g = Game("X X X X X X X X X X X X")
+    assert g.score() == 300
+
+def test_all_nine_and_miss():
+    g = Game("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-")
+    assert g.score() == 90
